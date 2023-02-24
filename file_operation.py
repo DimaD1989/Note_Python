0b66bdeb536a0909662183960b67f84e5587ef4f
DimaD1989 <d_d1989sv@mail.ru>
2023-02-24 11:48:33 +0300

    end

--- file_operation.py
+++ file_operation.py
@@ -1,7 +1,6 @@
 import Note
 
 
-
 def write_file(array, mode):
     file = open("notes.csv", mode='w', encoding='utf-8')
     file.seek(0)
@@ -27,47 +26,3 @@ def read_file():
         print('Нет сохраненных заметок...')
     finally:
         return array
-
-
-
-def show(text):
-    logic = True
-    array = file_operation.read_file()
-    if text == 'date':
-        date = input('Введите дату в формате dd.mm.yyyy: ')
-    for notes in array:
-        if text == 'all':
-            logic = False
-            print(Note.Note.map_note(notes))
-        if text == 'id':
-            logic = False
-            print('ID: ' + Note.Note.get_id(notes))
-        if text == 'date':
-            logic = False
-            if date in Note.Note.get_date(notes):
-                print(Note.Note.map_note(notes))
-    if logic == True:
-        print('Нет ни одной заметки...')
-
-
-def id_edit_del_show(text):
-    id = input('Введите id необходимой заметки: ')
-    array = file_operation.read_file()
-    logic = True
-    for notes in array:
-        if id == Note.Note.get_id(notes):
-            logic = False
-            if text == 'edit':
-                note = ui.create_note(number)
-                Note.Note.set_title(notes, note.get_title())
-                Note.Note.set_body(notes, note.get_body())
-                Note.Note.set_date(notes)
-                print('Заметка изменена...')
-            if text == 'del':
-                array.remove(notes)
-                print('Заметка удалена...')
-            if text == 'show':
-                print(Note.Note.map_note(notes))
-    if logic == True:
-        print('Такой заметки нет, возможно, вы ввели неверный id')
-    file_operation.write_file(array, 'a')
