ecd7ae7ef644e448c16bd7196dc3349d6bb68d90
DimaD1989 <d_d1989sv@mail.ru>
2023-02-24 15:27:54 +0300

    comments

--- file_operation.py
+++ file_operation.py
@@ -3,13 +3,14 @@ import Note
 
 def write_file(array, mode):
     file = open("notes.csv", mode='w', encoding='utf-8')
-    file.seek(0)
-    file.close()
+    file.seek(0) # Метод файла file.seek() устанавливает текущую позицию в байтах offset для указателя чтения/записи в файле file.
+    #0 - означает, что нужно сместить указатель на offset относительно начала файла.
+    file.close()  #Метод файла file.close() закрывает открытый файл.Закрытый файл больше не может быть прочитан или записан. Любая операция, которая требует, чтобы файл был открыт, вызовет исключение ValueError после того, как файл был закрыт. Вызов file.close() более одного раза разрешен.
     file = open("notes.csv", mode=mode, encoding='utf-8')
     for notes in array:
         file.write(Note.Note.to_string(notes))
         file.write('\n')
-    file.close
+    file.close #Метод файла file.close() закрывает открытый файл.Закрытый файл больше не может быть прочитан или записан. Любая операция, которая требует, чтобы файл был открыт, вызовет исключение ValueError после того, как файл был закрыт. Вызов file.close() более одного раза разрешен.
 
 
 def read_file():
