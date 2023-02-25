ba12e63a9120afe9433dfcd250b7d3ade5da192c
DimaD1989 <d_d1989sv@mail.ru>
2023-02-25 12:16:13 +0300

    redact command

--- controllers.py
+++ controllers.py
@@ -9,19 +9,19 @@ def run():
         input_from_user = input().strip()
         if input_from_user == '1':
             f.show('all')
-        if input_from_user == '2':
+        elif input_from_user == '2':
             f.add()
-        if input_from_user == '3':
+        elif input_from_user == '3':
             f.show('all')
             f.id_edit_del_show('del')
-        if input_from_user == '4':
+        elif input_from_user == '4':
             f.show('all')
             f.id_edit_del_show('edit')
-        if input_from_user == '5':
+        elif input_from_user == '5':
             f.show('date')
-        if input_from_user == '6':
+        elif input_from_user == '6':
             f.show('id')
             f.id_edit_del_show('show')
-        if input_from_user == '7':
+        elif input_from_user == '7':
             ui.goodbuy()
             break
